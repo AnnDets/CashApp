361b3c1ffd9af50dbe1dfb064a7ee4a79f3bd47b
Mornigun <dets_anna@mail.ru>
2025-12-06 15:24:28 +0300

    feat: add initial config

---
+++ src/main/java/edu/bsu/cashstorage/CashStorageApplication.java
@@ -0,0 +1,13 @@
+package edu.bsu.cashstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bsu/cashstorage/CashStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bsu.cashstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
