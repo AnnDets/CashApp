b40f2353423c0d490dabef55e6469f6f22718987
Mornigun <dets_anna@mail.ru>
2025-12-08 16:19:21 +0300

    fix: fix migrations

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -1,8 +1,9 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE
+EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE accounts
 (
-    id           UUID NOT NULL DEFAULT uuid_generate_v4(),
+    id                       UUID         NOT NULL DEFAULT uuid_generate_v4(),
     user_id                  UUID         NOT NULL,
     name                     VARCHAR(255) NOT NULL,
     type                     SMALLINT     NOT NULL,
@@ -12,10 +13,10 @@ CREATE TABLE accounts
     include_in_total_balance BOOLEAN      NOT NULL,
     default_account          BOOLEAN      NOT NULL,
     bank_id                  UUID,
-    card_number1 VARCHAR(100),
-    card_number2 VARCHAR(100),
-    card_number3 VARCHAR(100),
-    card_number4 VARCHAR(100),
+    card_number1             VARCHAR(100),
+    card_number2             VARCHAR(100),
+    card_number3             VARCHAR(100),
+    card_number4             VARCHAR(100),
     savings_account          BOOLEAN      NOT NULL,
     archive_account          BOOLEAN      NOT NULL,
     CONSTRAINT pk_accounts PRIMARY KEY (id)
@@ -23,7 +24,7 @@ CREATE TABLE accounts
 
 CREATE TABLE banks
 (
-    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    id           UUID           NOT NULL DEFAULT uuid_generate_v4(),
     country      VARCHAR(30)    NOT NULL,
     display_name VARCHAR(30)    NOT NULL,
     icon         VARCHAR(10000) NOT NULL,
@@ -45,7 +46,7 @@ CREATE TABLE categories
 
 CREATE TABLE colors
 (
-    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    id    UUID     NOT NULL DEFAULT uuid_generate_v4(),
     red   SMALLINT NOT NULL,
     green SMALLINT NOT NULL,
     blue  SMALLINT NOT NULL,
@@ -56,32 +57,33 @@ CREATE TABLE currencies
 (
     id           VARCHAR(3)  NOT NULL,
     display_name VARCHAR(30) NOT NULL,
-    symbol VARCHAR(2),
+    symbol       VARCHAR(2),
     CONSTRAINT pk_currencies PRIMARY KEY (id)
 );
 
 CREATE TABLE icons
 (
-    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    id   UUID          NOT NULL DEFAULT uuid_generate_v4(),
     data VARCHAR(5000) NOT NULL,
     CONSTRAINT pk_icons PRIMARY KEY (id)
 );
 
 CREATE TABLE operations
 (
-    id             UUID     NOT NULL DEFAULT uuid_generate_v4(),
-    external_system_operation_id VARCHAR(255)                NOT NULL,
+    id                           UUID         NOT NULL DEFAULT uuid_generate_v4(),
+    external_system_operation_id VARCHAR(255) NOT NULL,
+    user_id                      UUID         NOT NULL,
     category_id                  UUID,
     account_outcome_id           UUID,
     account_income_id            UUID,
-    operation_type SMALLINT NOT NULL,
+    operation_type               SMALLINT     NOT NULL,
     operation_date               TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     description                  VARCHAR(255),
     place_id                     UUID,
     income                       DECIMAL,
     outcome                      DECIMAL,
-    deleted                      BOOLEAN                     NOT NULL,
-    is_processed                 BOOLEAN                     NOT NULL,
+    deleted                      BOOLEAN      NOT NULL,
+    is_processed                 BOOLEAN      NOT NULL,
     created                      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     updated                      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     CONSTRAINT pk_operations PRIMARY KEY (id)
@@ -89,7 +91,7 @@ CREATE TABLE operations
 
 CREATE TABLE places
 (
-    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    id          UUID         NOT NULL DEFAULT uuid_generate_v4(),
     description VARCHAR(255) NOT NULL,
     author_id   UUID         NOT NULL,
     CONSTRAINT pk_places PRIMARY KEY (id)
@@ -127,6 +129,9 @@ ALTER TABLE categories
 
 ALTER TABLE categories
     ADD CONSTRAINT FK_CATEGORIES_ON_COLOR FOREIGN KEY (color_id) REFERENCES colors (id);
+
+ALTER TABLE operations
+    ADD CONSTRAINT FK_OPERATIONS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE operations
     ADD CONSTRAINT FK_OPERATIONS_ON_ACCOUNT_INCOME FOREIGN KEY (account_income_id) REFERENCES accounts (id);
